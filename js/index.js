12457517e16c4c72fa5164360fee474849133855
zzuni0421 <jmryu615@gmail.com>
2026-03-24 12:37:57 +0000

    a

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function goDiary(){
+    window.location.href = "diary.html";
+}
+
+function goTimer(){
+    window.location.href = "timer.html";
+}
+
+function goStopwatch(){
+    window.location.href = "stopwatch.html";
+}
+
+function goStudy(){
+    window.location.href = "study.html";
+}
+
+function goStretch(){
+    window.location.href = "stretch.html";
+}
